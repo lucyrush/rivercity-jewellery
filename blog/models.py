be2373190c8ef3f4f602d68f0e5e9cbadb651dd0
lucyrush <lucy@codeinstitute.net>
2020-12-03 21:53:31 +0000

    Not able to run migrations due to an error

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,8 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
