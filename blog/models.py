71ee2cd87de065bb3536743c4bc6104b89e7dd87
lucyrush <lucy@codeinstitute.net>
2020-12-03 21:31:49 +0000

    Made changes to blogs model

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,7 @@
 from django.db import models
 
 
-class Post(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-    )
-    body = models.TextField()
-
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    date = models.DateField()
