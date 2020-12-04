82dd41d17ceb06e36bc2a702325ea8b2ad24c1b5
lucyrush <lucy@codeinstitute.net>
2020-12-04 18:43:11 +0000

    Added blog details page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from blog.views import blog, blog_detail
+
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,15 @@ def blog(request):
     posts = Post.objects.all()
 
     context = {
-        'posts':posts
+        'posts': posts
     }
     return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/blog-detail.html', context)
