62b7801eeaf6282655e82c6bfc09a4a21c5e61b5
lucyrush <lucy@codeinstitute.net>
2020-12-04 16:27:34 +0000

    Add new blog model and update admin

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+class Category(models.Model):
+    title = models.CharField(max_length=225)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, related_name="posts", on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
